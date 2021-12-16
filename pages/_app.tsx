fdea727661cf89c68c79d56913adcb81bc093565
C. Lewis <ctj.lewis@icloud.com>
2021-12-16 02:40:25 -0600

    fix: suppress helpful error
    Details: `Expected server HTML to contain a matching <h1> in <div>`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,33 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
 
-  if (typeof window === "undefined") return null;
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/about">
-          <h1>About</h1>
-        </Route>
-        <Route path="/topics">
-          <h1>Topics</h1>
-        </Route>
-        <Route path="/">
-          <h1>Home</h1>
-        </Route>
-      </Switch>
+    mounted &&
+      <BrowserRouter>
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/topics">
+            <h1>Topics</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
 
-      <div suppressHydrationWarning>
-        <Component {...pageProps} />
-      </div>
-    </BrowserRouter>
+        <div suppressHydrationWarning>
+          <Component {...pageProps} />
+        </div>
+      </BrowserRouter>
   );
 };
 export default App;
