96a84fdfc5b686faef93f790243c19d917cc0585
C. Lewis <ctj.lewis@icloud.com>
2021-12-16 04:21:54 -0600

    docs: comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 /**
  * A single-page application with full next/link support.
  */
-const SPA = ({ Component, pageProps }: AppProps) => {
-  const [mounted, setMounted] = useState(false);
+const SPA = ({ Component: PageComponent, pageProps }: AppProps) => {
   /**
-   * Whether or not the component is rendering in the browser.
+   * Mark the component as mounted once it has rendered in the browser.
    */
+  const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   /**
    * If the component is being rendered in the browser, it is wrapped with a
@@ -18,8 +18,8 @@ const SPA = ({ Component, pageProps }: AppProps) => {
   return (
     mounted &&
       <BrowserRouter>
-        {/* Render the given component. */}
-        <Component {...pageProps} />
+        {/* Render the given page. */}
+        <PageComponent {...pageProps} />
       </BrowserRouter>
   );
 };
